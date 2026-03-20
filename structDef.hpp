3de741762e337cab7e16ba8d04d312dc2514ca0e
Lucas Perrenoud <lucasmacp03@gmail.com>
2026-03-20 11:44:48 -0600

    improve struct definitions
    added values to task structs to keep track of preemptions, deadline misses,
    remaining work to be done, priority, and for periodic tasks, next release time

--- structDef.hpp
+++ structDef.hpp
@@ -7,12 +7,29 @@ struct taskPeriodic {
     std::string id;
     int compTime;
     int period;
+
+    int remainingTime = 0;
+    int nextRelease = 0;
+    int deadline = 0;
+
+    int preemptions = 0;
+    int deadlineMisses = 0;
+
+    int priority;
 };
 
 struct taskAperiodic {
     std::string id;
     int compTime;
     int releaseTime;
+
+    int remainingTime = 0;
+    int deadline = 0;
+
+    int preemptions = 0;
+    int deadlineMisses = 0;
+
+    int priority;
 };
 
 #endif
